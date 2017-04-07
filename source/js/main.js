deb2c5d5fdb49e5a0a8dfac1544b923e812cc742
Lucas Henrique <lucashenrique.he@gmail.com>
2017-04-07 00:20:43 -0300

    Updating first section of website

---
+++ source/js/main.js
@@ -0,0 +1,30 @@
+var App = App || {};
+
+;(function() {
+
+	App.Main = {
+
+		init: function() {
+			this.cacheSelectors();
+			this.bindEvents();
+		},
+
+		cacheSelectors: function() {
+
+		},
+
+		bindEvents: function() {
+
+		},
+
+		Events: {
+
+
+
+		}
+
+	}
+
+})();
+
+App.Main.init();
